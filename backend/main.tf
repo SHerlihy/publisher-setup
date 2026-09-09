6dbc4cad33bb4753f584c45f1774e05a09ae11c3
Steven Herlihy <steven_herlihy@yahoo.com>
2026-09-09 19:30:09 +0100

    draft backend s3 for current

---
+++ backend/main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_version = ">= 1.0, < 2.0"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = ">= 6.0, < 7.0"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "publisher_admin"
+  region = "eu-west-2"
+}
+
+locals {
+  common_tags = {
+    product_id = "quota endpoint"
+    facet = "admin"
+  }
+}
+
+resource "aws_s3_bucket" "backend" {
+  bucket_prefix = "publisher-setup-"
+
+  tags = merge(local.common_tags, {})
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+output "name" {
+  value = aws_s3_bucket.backend.name
+}
